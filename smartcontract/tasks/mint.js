47e402425dde1b93bfd47cc71e6f7be8a23c024e
vngnamlv <namlv@vng.com.vn>
2022-11-23 10:04:17 +0700

    edit 1155

--- smartcontract/tasks/mint.js
+++ smartcontract/tasks/mint.js
@@ -54,22 +54,20 @@ task("mint1155", "Mint 1155")
     }
 });
 
-task("mint1155Ver2", "Mint 1155")
+task("mint1155Ver3", "Mint 1155")
   .addPositionalParam("contractAddress", "address of contract", "", types.string)
   .addPositionalParam("tokenId", "tokenId", 1, types.int)
   .addPositionalParam("number", "number of nfts", 1, types.int)
+  .addPositionalParam("for", "address of user", "", types.string)
   .setAction(async (taskArgs, hre) => {
     const [deployer] = await hre.ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
     const erc1155 = await hre.ethers.getContractFactory("VNGHero1155");
     const erc1155Instance = erc1155.attach(taskArgs.contractAddress);
-    
-    console.log(`Mint NFT of ${taskArgs.contractAddress}`);
-    for(i = 1; i <= taskArgs.number; i++){
-      console.log(`${i} Minting....`)
-      let rs = await erc1155Instance.mintVer2(taskArgs.tokenId, taskArgs.number);
-      await rs.wait();
-    }
+
+    console.log(`Minting....`)
+    let rs = await erc1155Instance.mintVer3(taskArgs.for == "" ? deployer.address : taskArgs.for, taskArgs.tokenId, taskArgs.number);
+    await rs.wait();
 });
 
 task("mintERC20", "Mint 20")
